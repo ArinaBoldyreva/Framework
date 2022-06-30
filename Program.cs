942aea91b4166b4d416ce3b942a71f45b2043bbc
ArinaBoldyreva <100853471+ArinaBoldyreva@users.noreply.github.com>
2022-06-30 15:31:47 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using static System.Console;
+
+namespace Exercise05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("x = 3 << 2;");
+            WriteLine("y = 10 >> 1;");
+
+            int x = 3 << 2;
+            int y = 10 >> 1;
+            WriteLine($"x = {x} и y = {y}");
+            WriteLine();
+
+        }
+    }
+}
+

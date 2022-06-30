c0e2041ff582cc5c04e8003c7b9a1522f066e61f
ArinaBoldyreva <100853471+ArinaBoldyreva@users.noreply.github.com>
2022-06-30 15:30:57 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using static System.Console;
+
+namespace Exercise04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("x = 10 & 8;");
+            WriteLine("y = 10 | 7;");
+
+            int x = 10 & 8;
+            int y = 10 | 7;
+            WriteLine($"x = {x} и y = {y}");
+            WriteLine();
+                
+        }
+    }
+}
+
+
+
